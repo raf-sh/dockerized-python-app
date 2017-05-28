6462606d7f9c3da91f28c7f826db1c7ab75cf36a
Rafail <ahmetsha@gmail.com>
2017-05-28 22:33:21 +0200

    draft index page

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Dockersized Flask is running'
+    return render_template(
+        'index.html',
+        title='Dockerized Flask is running'
+    )
+
+app.run(debug=True)
